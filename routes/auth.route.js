b5ab4f5456586b7088b05008784f08f23cff1a29
Uzair Ali <ghayasali2424@gmail.com>
2026-01-31 23:14:55 +0500

    protected routes

--- routes/auth.route.js
+++ routes/auth.route.js
@@ -1,13 +1,13 @@
 const { bodyValidator } = require("../middlewares/validation.middleware");
-const { checkUserExists } = require("../middlewares/authorization.middleware");
-const { signupSchema } = require("../validation/auth.validation");
+const { checkUserExists, authenticate } = require("../middlewares/authorization.middleware");
+const { signupSchema, loginSchema } = require("../validation/auth.validation");
 const { AuthController } = require("../controllers/auth.controller");
 
 const { Router } = require("express");
-const { loginSchema } = require("../../backend-02/validators/validator");
 const router = Router();
 
 router.post("/sign-up", bodyValidator(signupSchema), checkUserExists, AuthController.signup);
 router.post("/login", bodyValidator(loginSchema), AuthController.login);
+router.post("/logout", authenticate, AuthController.logout);
 
 module.exports = router;
