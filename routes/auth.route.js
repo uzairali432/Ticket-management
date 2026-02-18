9787f06371d778402af0e516581bf1e4d1020965
Uzair Ali <ghayasali2424@gmail.com>
2026-02-18 23:47:21 +0500

    only admin can add user

--- routes/auth.route.js
+++ routes/auth.route.js
@@ -1,12 +1,19 @@
 const { bodyValidator } = require("../middlewares/validation.middleware");
-const { checkUserExists, authenticate } = require("../middlewares/authorization.middleware");
+const { checkUserExists, authenticate, authorizeAdmin } = require("../middlewares/authorization.middleware");
 const { signupSchema, loginSchema } = require("../validation/auth.validation");
 const { AuthController } = require("../controllers/auth.controller");
 
 const { Router } = require("express");
 const router = Router();
 
-router.post("/sign-up", bodyValidator(signupSchema), checkUserExists, AuthController.signup);
+router.post(
+	"/sign-up",
+	authenticate,
+	authorizeAdmin,
+	bodyValidator(signupSchema),
+	checkUserExists,
+	AuthController.signup,
+);
 router.post("/login", bodyValidator(loginSchema), AuthController.login);
 router.post("/logout", authenticate, AuthController.logout);
 
